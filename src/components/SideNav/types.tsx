9735f7451bb53320d070b7ab273bddc363c0d0b4
Mark <m.bae@live.com>
2020-04-12 18:03:22 -0400

    refactor sidebar files and fix bug for chrome animations

---
+++ src/components/SideNav/types.tsx
@@ -0,0 +1,11 @@
+export type NavItemData = {
+  to: string;
+  label: string;
+  iconURL: string;
+  title: string;
+};
+export type ControlMenuAction = {
+  label: string;
+  iconURL: string;
+  title: string;
+};
